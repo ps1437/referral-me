1d1a23b162579469a2229beabaaa68c0f2e4fc62
Praveen Soni <31918440+ps1437@users.noreply.github.com>
2024-05-19 18:56:45 +0530

    Create README.md

---
+++ referral-me-ui/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="w-full flex flex-col sm:flex-row flex-grow overflow-hidden">
+    <div className="sm:w-1/3 md:1/4 w-full flex-shrink flex-grow-0 p-4">
+        <div className="sticky top-0 p-4 w-full">
+            <ul className="flex sm:flex-col overflow-hidden content-center justify-between">
+            </ul>
+        </div>
+    </div>
+    <main role="main" className="w-full h-full flex-grow p-3 overflow-auto">
+      DDDDDDDDDDDDDDDDDDDDDDDDDDdd
+    </main>
+</div>
+<footer className="mt-auto">
+ ddddddddddddddddddddddddddddd>>
+</footer>
+</>
+  );
+}
