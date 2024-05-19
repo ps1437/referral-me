4197ea826d124f195a1ef0d63db47e88f9c0c722
praveen <praveen369soni@gmail.com>
2024-05-19 18:56:01 +0530

    project setup

---
+++ referral-me-api/src/test/java/com/syshco/referral/ReferralMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syshco.referral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferralMeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
